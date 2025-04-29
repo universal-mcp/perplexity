006861202f96887faa77086bcf1730f1a5e12fdb
Manoj Bajaj <manojbajaj95@gmail.com>
2025-04-29 22:02:28 +0530

    feat: Initial commit from MCP CLI init command

---
+++ src/universal_mcp_perplexity/mcp.py
@@ -0,0 +1,19 @@
+
+from universal_mcp.servers.server import SingleMCPServer
+from universal_mcp.integrations.agentr import AgentRIntegration
+from universal_mcp.stores.store import EnvironmentStore
+
+from universal_mcp_perplexity.app import PerplexityApp
+
+env_store = EnvironmentStore()
+integration_instance = AgentRIntegration(name="perplexity", store=env_store)
+app_instance = PerplexityApp(integration=integration_instance)
+
+mcp = SingleMCPServer(
+    app_instance=app_instance,
+)
+
+if __name__ == "__main__":
+    mcp.run()
+
+
